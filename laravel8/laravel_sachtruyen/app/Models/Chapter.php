f73efffeba4a8464a54296235b5f13e9899f1185
HoaiTrang0305 <2014496@dlu.edu.vn>
2023-04-13 22:08:03 +0700

    TrangChu

--- laravel8/laravel_sachtruyen/app/Models/Chapter.php
+++ laravel8/laravel_sachtruyen/app/Models/Chapter.php
@@ -5,17 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Truyen extends Model
+class Chapter extends Model
 {
     use HasFactory;
     public $timestamps=false;
     protected $fillable=[
-        'tentruyen','tomtat','kichhoat','slug_truyen','hinhanh','danhmuc_id'
+        'truyen_id','tomtat','tieude','noidung','kichhoat','slug_chapter'
     ];
     protected $primaryKey='id';
-    protected $table='truyen';
+    protected $table='chapter';
 
-    public function danhmuctruyen(){
-        return $this->belongsTo('App\Models\DanhmucTruyen','danhmuc_id','id');
+    public function truyen(){
+        return $this->belongsTo('App\Models\Truyen');
     }
 }
--- laravel8/laravel_sachtruyen/routes/web.php
+++ laravel8/laravel_sachtruyen/routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TruyenController;
 use App\Http\Controllers\DanhmucController;
+use App\Http\Controllers\ChapterController;
+use App\Http\Controllers\IndexController;
+
 
 
 /*
@@ -17,9 +20,9 @@ use App\Http\Controllers\DanhmucController;
 |
 */
 
-Route::get('/', function () {
-    return view('layout');
-});
+Route::get('/', [IndexController::class,'home']);
+Route::get('/danh-muc/{slug}', [IndexController::class,'danhmuc']);
+Route::get('/xem-truyen/{slug}', [IndexController::class,'xemtruyen']);
 
 Auth::routes();
 
@@ -27,5 +30,7 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::resource('/danhmuc', DanhmucController::class);
 Route::resource('/truyen', TruyenController::class);
+Route::resource('/chapter', ChapterController::class);
+
 
 
